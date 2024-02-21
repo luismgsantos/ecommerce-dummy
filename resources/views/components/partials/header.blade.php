e19161f617e870156bc592e0a8e716b3dc63abc4
Luís Santos <luis.santos.ext@tlscontact.com>
2024-02-21 16:37:35 +0000

    chore: remove unused header links

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -5,14 +5,9 @@
             <div class="flex items-center justify-between flex-grow md:pl-12 py-5">
                 <div class="flex items-center space-x-6 capitalize">
                     <a href="{{ route('home') }}" class="text-gray-200 hover:text-white transition">Home</a>
-                    <a href="pages/shop.html" class="text-gray-200 hover:text-white transition">Shop</a>
-                    <a href="#" class="text-gray-200 hover:text-white transition">About us</a>
-                    <a href="#" class="text-gray-200 hover:text-white transition">Contact us</a>
                 </div>
 
                 <x-header.search-bar />
-
-                <a href="pages/login.html" class="text-gray-200 hover:text-white transition">Login</a>
             </div>
         </div>
     </nav>
