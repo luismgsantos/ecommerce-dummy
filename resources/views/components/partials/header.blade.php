8e315e2f92ebde4a41b64b9c882fe9bd514cff5c
Luís Santos <luis.santos.ext@tlscontact.com>
2024-02-21 14:48:26 +0000

    fix: home route on homepage

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -4,7 +4,7 @@
             <x-header.category-dropdown />
             <div class="flex items-center justify-between flex-grow md:pl-12 py-5">
                 <div class="flex items-center space-x-6 capitalize">
-                    <a href="index.html" class="text-gray-200 hover:text-white transition">Home</a>
+                    <a href="{{ route('home') }}" class="text-gray-200 hover:text-white transition">Home</a>
                     <a href="pages/shop.html" class="text-gray-200 hover:text-white transition">Shop</a>
                     <a href="#" class="text-gray-200 hover:text-white transition">About us</a>
                     <a href="#" class="text-gray-200 hover:text-white transition">Contact us</a>
