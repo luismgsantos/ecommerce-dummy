1eea57f4623cfc07e388059f7a405fefdd1042fe
Luís Santos <luis.santos.ext@tlscontact.com>
2024-02-21 14:50:12 +0000

    feat: added top level categories main page

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -12,8 +12,19 @@
     <x-partials.header />
 
     <main>
-        <x-partials.categories.picker :subcategories="$category->subcategories" />
+        <x-partials.categories.picker :title="$category->name" :subcategories="$category->subcategories" />
+
+        <x-partials.product-grid :title="$category->icon . $category->name" :products="$category->products"/>
+
+        <div class="container pb-16 mx-auto">
+            <h2 class="text-2xl font-medium text-gray-800 uppercase mb-6">You might also like</h2>
+        </div>
+
+        @foreach ($category->subcategories as $subcategory)
+            <x-partials.product-grid :title="$subcategory->icon . $subcategory->name" :products="$subcategory->products"/>
+        @endforeach
     </main>
+
     <x-partials.footer />
 
     <x-partials.copyright />
