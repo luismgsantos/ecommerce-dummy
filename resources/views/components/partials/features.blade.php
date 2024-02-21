a57662ddd43475b82c1dd075d38fe5fa0c30d3b7
Luís Santos <luis.santos.ext@tlscontact.com>
2024-02-21 14:47:35 +0000

    chore: cleanup and typo fix

--- resources/views/components/partials/features.blade.php
+++ resources/views/components/partials/features.blade.php
@@ -4,13 +4,13 @@
             <img src="{{ asset('assets/images/icons/delivery-van.svg') }}" alt="Delivery" class="w-12 h-12 object-contain">
             <div>
                 <h4 class="font-medium capitalize text-lg">Free Shipping</h4>
-                <p class="text-gray-500 text-sm">Order over $200</p>
+                <p class="text-gray-500 text-sm">Order over 200€</p>
             </div>
         </div>
         <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
             <img src="{{ asset('assets/images/icons/money-back.svg') }}" alt="Delivery" class="w-12 h-12 object-contain">
             <div>
-                <h4 class="font-medium capitalize text-lg">Money Rturns</h4>
+                <h4 class="font-medium capitalize text-lg">Money Returns</h4>
                 <p class="text-gray-500 text-sm">30 days money returs</p>
             </div>
         </div>
