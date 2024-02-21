26ba4653af2bf0a81ca3bfbcaa45f95cc9efe7b6
Luís Santos <luis.santos.ext@tlscontact.com>
2024-02-21 14:47:19 +0000

    feat: added dynamic banned for highlighted category

--- resources/views/components/partials/banner.blade.php
+++ resources/views/components/partials/banner.blade.php
@@ -1,13 +1,11 @@
-<div class="bg-cover bg-no-repeat bg-center py-36" style="background-image: url('{{ asset('assets/images/banner-bg.jpg') }}');">
+<div class="bg-cover bg-no-repeat bg-center py-36" style="background-image: url('{{ $category->image }}');">
     <div class="container mx-auto">
         <h1 class="text-6xl text-gray-800 font-medium mb-4 capitalize">
-            best collection for <br> home decoration
+            {{ $category->name }}
         </h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam <br>
-            accusantium perspiciatis, sapiente
-            magni eos dolorum ex quos dolores odio</p>
+        <p>{{ $category->description }}</p>
         <div class="mt-12">
-            <a href="#"
+            <a href="{{ route('category', $category) }}"
                 class="border border-primary text-white bg-gray-800 px-8 py-3 font-medium
                     rounded-md hover:bg-white hover:text-gray-800">
                 Shop Now
