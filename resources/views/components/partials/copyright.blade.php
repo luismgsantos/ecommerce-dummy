20f1edc8441d1893d96b85f55167929af0ef7283
Luís Santos <luis.santos.ext@tlscontact.com>
2024-02-21 15:26:25 +0000

    feat: added mention to the creator of the html template

--- resources/views/components/partials/copyright.blade.php
+++ resources/views/components/partials/copyright.blade.php
@@ -1,8 +1,7 @@
 <div class="bg-gray-800 py-4">
     <div class="container flex items-center justify-between mx-auto">
-        <p class="text-white">&copy; TailCommerce - All Right Reserved</p>
-        <div>
-            <img src="{{ asset('assets/images/methods.png') }}" alt="methods" class="h-5">
-        </div>
+        <p class="text-white">
+            Special thanks to Fajar for the <a href="https://github.com/fajar7xx/ecommerce-template-tailwind-1" target="blank">HTML template</a>
+        </p>
     </div>
 </div>
