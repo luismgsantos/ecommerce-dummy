4f3311817841a54286a31615dd657af96a83e54e
Luís Santos <luis.santos.ext@tlscontact.com>
2024-02-22 23:44:05 +0000

    feat: Added banner to Category and Subcategories page

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -1,13 +1,5 @@
 <x-layout.app>
-    <x-partials.categories.picker :title="$category->name" :subcategories="$category->subcategories" />
+    <div class="py-24 bg-fixed bg-no-repeat bg-cover"
+        style="background-image: url('{{ $category->image }}')"></div>
 
-    <x-partials.product-grid :title="$category->icon . $category->name" :products="$category->products" />
-
-    <div class="container pb-16 mx-auto">
-        <h2 class="text-2xl font-medium text-gray-800 uppercase mb-6">You might also like</h2>
-    </div>
-
-    @foreach ($category->subcategories as $subcategory)
-        <x-partials.product-grid :title="$subcategory->icon . $subcategory->name" :products="$subcategory->products" />
-    @endforeach
 </x-layout.app>
--- resources/views/pages/subcategory.blade.php
+++ resources/views/pages/subcategory.blade.php
@@ -1,4 +1,4 @@
 <x-layout.app>
-    <x-partials.categories.picker :title="'Other subcategories you might like'" :subcategories="$subcategory->category->subcategories" />
-    <x-partials.product-grid :title="$subcategory->icon . $subcategory->name" :products="$subcategory->products" />
+    <div class="py-24 bg-fixed bg-blend-lighten bg-no-repeat bg-cover"
+        style="background-image: url('{{ $subcategory->image }}')"></div>
 </x-layout.app>
