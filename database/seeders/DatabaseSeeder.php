4fe5fdd708eb1bacdb0e9cf6cc7e27568e900c49
Luís Santos <luis.santos.ext@tlscontact.com>
2024-02-22 23:41:42 +0000

    chore: Remove unused model imports in DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Category;
 use App\Models\Product;
-use App\Models\Subcategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
