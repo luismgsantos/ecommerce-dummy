90b840f1343aa7a025782997d05c053de7b3349c
Luís Santos <luis.santos.ext@tlscontact.com>
2024-02-22 23:43:17 +0000

    feat: Update category grid layout and product grid title

--- resources/views/components/partials/categories.blade.php
+++ resources/views/components/partials/categories.blade.php
@@ -1,9 +1,9 @@
 <div class="container py-16 mx-auto">
     <h2 class="text-2xl font-medium text-gray-800 uppercase mb-6">shop by category</h2>
-    <div class="grid grid-cols-3 gap-3">
+    <div class="grid grid-cols-4 gap-3">
         @foreach ($categories as $category)
             <div class="relative rounded-sm overflow-hidden group">
-                <img src="{{ $category->image }}" alt="{{ $category->name }}" class="w-full">
+                <img src="{{ $category->image }}" alt="{{ $category->name }}" class="w-full object-contain hover:object-scale-down">
                 <a href="{{ route('category', $category->slug) }}"
                     class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">
                     {{ $category->name }}
--- resources/views/components/partials/product-grid.blade.php
+++ resources/views/components/partials/product-grid.blade.php
@@ -1,5 +1,7 @@
-<div class="container pb-16 mx-auto">
-    <h2 class="text-2xl font-medium text-gray-800 uppercase mb-6">{{ $title }}</h2>
+<div class="container pb-16 pt-8 mx-auto">
+    <div class="flex justify-between items-center py-3 mb-6">
+        <h2 class="text-2xl font-medium text-gray-800 uppercase items-center">{{ $title }}</h2>
+    </div>
     <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
         @foreach ($products as $product)
             <x-product.product-card :product="$product" />
