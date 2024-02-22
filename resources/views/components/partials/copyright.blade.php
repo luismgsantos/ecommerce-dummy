c0f510c01f062e7eabe2d189bf92b54a435e3b28
Luís Santos <luis.santos.ext@tlscontact.com>
2024-02-22 14:31:29 +0000

    feat: improved copy text in footer

--- resources/views/components/partials/copyright.blade.php
+++ resources/views/components/partials/copyright.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-gray-800 py-4">
     <div class="container flex items-center justify-between mx-auto">
         <p class="text-white">
-            Special thanks to Fajar for the <a href="https://github.com/fajar7xx/ecommerce-template-tailwind-1" target="blank">HTML template</a>
+            Thank you to Fajar for the <a href="https://github.com/fajar7xx/ecommerce-template-tailwind-1" target="blank">HTML template</a>
         </p>
     </div>
 </div>
--- resources/views/components/partials/footer.blade.php
+++ resources/views/components/partials/footer.blade.php
@@ -4,67 +4,15 @@
             <icon>🚧🖥️</icon>
             <div class="mr-2">
                 <p class="text-gray-500">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, hic?
+                    Buying just became easier with our new online store. We are open 24/7 and we deliver fictional
+                    products to your doorstep.
                 </p>
-            </div>
-            <div class="flex space-x-5">
-                <a href="#" class="text-gray-400 hover:text-gray-500">
-                    F
-                </a>
-                <a href="#" class="text-gray-400 hover:text-gray-500">
-                    I
-                </a>
-                <a href="#" class="text-gray-400 hover:text-gray-500">
-                    X
-                </a>
-                <a href="#" class="text-gray-400 hover:text-gray-500">
-                    G
-                </a>
-            </div>
-        </div>
-
-        <div class="col-span-2 grid grid-cols-2 gap-4">
-            <div class="grid grid-cols-2 gap-4 md:gap-8">
-                <div>
-                    <h3 class="text-sm font-semibold text-gray-400 uppercase tracking-wider">Solutions</h3>
-                    <div class="mt-4 space-y-4">
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Marketing</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Analitycs</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Commerce</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Insights</a>
-                    </div>
-                </div>
-
-                <div>
-                    <h3 class="text-sm font-semibold text-gray-400 uppercase tracking-wider">Support</h3>
-                    <div class="mt-4 space-y-4">
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Pricing</a>
-                        <!-- <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Documentation</a> -->
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Guides</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">API Status</a>
-                    </div>
-                </div>
-            </div>
-            <div class="grid grid-cols-2 gap-8">
-                <div>
-                    <h3 class="text-sm font-semibold text-gray-400 uppercase tracking-wider">Solutions</h3>
-                    <div class="mt-4 space-y-4">
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Marketing</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Analitycs</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Commerce</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Insights</a>
-                    </div>
-                </div>
-
-                <div>
-                    <h3 class="text-sm font-semibold text-gray-400 uppercase tracking-wider">Support</h3>
-                    <div class="mt-4 space-y-4">
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Pricing</a>
-                        <!-- <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Documentation</a> -->
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Guides</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">API Status</a>
-                    </div>
-                </div>
+                <p class="text-gray-400 mt-4">
+                    @php
+                        Artisan::call('inspire');
+                        echo Artisan::output();
+                    @endphp
+                </p>
             </div>
         </div>
     </div>
